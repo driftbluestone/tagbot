fc9cb79fb85b37136f0f643483bed5098f1c4948
driftbluestone <driftbluestone@gmail.com>
2026-02-24 20:19:23 -0800

    edit delete logging

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import discord, os, pathlib
 from discord.ext import commands
 from modules import tags, sed, sonny, message_embed
+from modules.config import *
 bot = commands.Bot(
-    command_prefix="%",
+    command_prefix="$",
     allowed_mentions=discord.AllowedMentions(
         users=False,
         everyone=False,
@@ -13,7 +14,7 @@ bot = commands.Bot(
     intents=discord.Intents.all(),
 )
 DIR = pathlib.Path(__file__).resolve().parent
-
+channel = 0
 if not os.path.isdir(f"{DIR}/tags"):
     os.mkdir(f"{DIR}/tags")
 if not os.path.isdir(f"{DIR}/users"):
@@ -24,10 +25,13 @@ with open(f"{DIR}/TOKEN.txt", "r") as file:
 
 @bot.event
 async def on_ready():
+    global channel
+    channel = await bot.fetch_channel(server_config["edit_delete_log_channel"])
     print(f"Logged in as {bot.user}.")
 
 @bot.event
 async def on_message(message: discord.Message):
+    
     # Keep commands working
     if message.content.startswith(f"{bot.command_prefix}"):
         return await bot.process_commands(message)
@@ -41,6 +45,30 @@ async def on_message(message: discord.Message):
     #sed command
     if message.content.startswith("sed/"):
         await sed.sed(message)
+
+@bot.event
+async def on_message_edit(previous: discord.Message, current: discord.Message):
+    if previous.author.bot:
+        return
+    prev = previous.content
+    cur = current.content
+    embed = discord.Embed(title=f"Message edited in <#{previous.channel.id}>", description=f"**Before:** `{prev}`\n**After:** `{cur}`\n[Jump to message]({current.jump_url})")
+    embed.timestamp = previous.created_at
+    embed.set_footer(text=f"ID: {current.id}")
+    embed.set_author(name=previous.author.name, icon_url=previous.author.avatar)
+    await channel.send(embed=embed)
+
+@bot.event
+async def on_message_delete(message: discord.Message):
+    if message.author.bot:
+        return
+    content = message.content
+    embed = discord.Embed(title=f"Message deleted in <#{message.channel.id}>", description=f"**Content:** `{content}`")
+    embed.timestamp = message.created_at
+    embed.set_footer(text=f"ID: {message.id}")
+    embed.set_author(name=message.author.name, icon_url=message.author.avatar)
+    await channel.send(embed=embed)
+    
 
 @bot.command(name="tag")
 async def tag(ctx):
