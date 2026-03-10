d2ed80bd1f6e931d7f6e94f1e5233bb018e3eb38
driftbluestone <driftbluestone@gmail.com>
2026-03-09 21:53:29 -0700

    custom emoji support

--- cogs/boards.py
+++ cogs/boards.py
@@ -15,9 +15,10 @@ class Boards(commands.Cog):
     
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, user: discord.Member):
-        if reaction.emoji not in server_config["boards"].keys(): return
-        if reaction.count != server_config["boards"][reaction.emoji][1]: return
-        channel = await self.bot.fetch_channel(server_config["boards"][reaction.emoji][0])
+        emoji = str(reaction.emoji)
+        if emoji not in server_config["boards"].keys(): return
+        if reaction.count != server_config["boards"][emoji][1]: return
+        channel = await self.bot.fetch_channel(server_config["boards"][emoji][0])
         await reaction.message.forward(channel)
 
 async def setup(bot: commands.Bot) -> None:
