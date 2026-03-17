7cb6fb9c61e42025c54dcecc16cc3a08a8cd64c5
driftbluestone <driftbluestone@gmail.com>
2026-03-16 22:15:36 -0700

    fix tag owner

--- modules/tags/functions.py
+++ modules/tags/functions.py
@@ -158,6 +158,6 @@ async def tag_list(ctx: commands.Context, message: list):
 async def tag_owner(ctx: commands.Context, message: list):
     tag: str = message[0]
     if not tag: return await ctx.reply(":information_source: %t owner `tag`")
-    data = await tag_utils.get_tag_data(ctx, tag, True, False)[0]
+    data = (await tag_utils.get_tag_data(ctx, tag))[0]
     if not data: return await ctx.reply(f":warning: Tag **{tag}** does not exist.")
     return await ctx.reply(f":information_source: Tag **{tag}** is owned by <@{data["owner"]}>.")
