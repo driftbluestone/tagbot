c471c84c69a51c96f8dd572a69fd40b987c00338
driftbluestone <driftbluestone@gmail.com>
2026-03-16 23:32:35 -0700

    admin function calls

--- modules/tags/tags.py
+++ modules/tags/tags.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-from modules.tags import users, functions
+from modules.tags import users, functions, admin_functions
 DIR = Path(__file__).resolve().parent.parent.parent
 
 SPECIAL_TAGS = ["add", "edit", "delete", "alias", "list", "owner", "search", "raw"]
 DISPLAYED_SPECIAL_TAGS = ["add", "edit", "delete", "alias", "list", "owner", "search"]
 ADMIN_TAGS = ["delete", "promote", "limit", "ban"]
-
+DISPLAYED_ADMIN_TAGS = ["delete", "promote", "limit", "ban", "edit"]
 
 async def context_formatter(ctx):
     message = ctx.message.content
@@ -23,15 +23,22 @@ async def get_tag(ctx, tag, message):
         return await ctx.reply(f":information_source: %t `{"|".join(DISPLAYED_SPECIAL_TAGS)}`")
     tag.lower()
     if tag in SPECIAL_TAGS:
-        if message == []:
-            message = ["", ""]
+        if message == []: message = ["", ""]
         message[0].lower
         action =  getattr(functions, f"tag_{tag}")
         return await action(ctx, message)
     if tag == "admin":
-        pass
+        return await admin_tag(ctx, message[0].lower, message[1:])
+
+async def admin_tag(ctx, tag, message):
+    if not await users.permission_check(ctx.author, "tag_admin"): return await ctx.reply(":warning: No permission.")
+    if message == []: message = ["", ""]
+    tag.lower
+    if (not tag) or ( tag not in ADMIN_TAGS):
+        return await ctx.reply(f":information_source: %t `{"|".join(DISPLAYED_ADMIN_TAGS)}`")
+    action =  getattr(admin_functions, f"admin_{tag}")
+    return await action(ctx, message)
+
     
-# elif tag == "raw":
-#     await raw_tag(ctx, message[0])
 # elif tag == "admin":
 #     await admin_tag(ctx, message[0], message[1:])
