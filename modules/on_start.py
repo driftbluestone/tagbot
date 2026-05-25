c94026499a72a5a7ed0adbb1b8f9671226426565
driftbluestone <driftbluestone@gmail.com>
2026-05-24 22:56:56 -0700

    fix initialization bug

--- modules/on_start.py
+++ modules/on_start.py
@@ -2,6 +2,8 @@ import pathlib, os
 DIR = pathlib.Path(__file__).resolve().parent.parent
 
 def on_ready():
+    if not os.path.isdir(f"{DIR}/data"):
+        os.mkdir(f"{DIR}/data")
     if not os.path.isdir(f"{DIR}/data/install_logs"):
         os.mkdir(f"{DIR}/data/install_logs")
     if not os.path.isdir(f"{DIR}/data/users"):
