baa9c01afb209517935a96a4d7207e6c0f82c4bd
driftbluestone <driftbluestone@gmail.com>
2026-06-08 18:39:02 -0700

    fix ExtensionManager bug

--- cogs/extensions.py
+++ cogs/extensions.py
@@ -125,7 +125,7 @@ class Installer:
     
 class ExtensionManager(gui.MenuGUI):
     """Manage which extensions are enabled or disabled via /extension toggle"""
-    def __init__(self, interaction, _, page = 1):
+    def __init__(self, interaction, _ = None, page = 1):
         super().__init__(interaction=interaction, element_count=len(server_config["extensions"].keys()), page=page)
         extensions = list(server_config["extensions"].keys())
         extensions = extensions[((self.page-1)*10):(self.page*10)]
