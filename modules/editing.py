8c01334cce983224850b58c76779d3d3e83617dc
driftbluestone <117239036+driftbluestone@users.noreply.github.com>
2026-06-11 22:58:11 -0700

    all json calls in modules replaced with jsonIO

--- modules/editing.py
+++ modules/editing.py
@@ -1,19 +1,18 @@
-import discord, pathlib, json, os
-from utils import message_embed
+import discord, pathlib, os
+from utils import message_embed, jsonIO
 DIR = pathlib.Path(__file__).resolve().parent.parent
 
 async def new_edit(message: discord.Message, deleted = False):
     id = str(message.id)
     filepath = f"{DIR}/data/history/{id}.json"
     if pathlib.Path(filepath).exists():
-        with open(filepath, "r") as file:
-            reply_id = json.load(file)
+        reply_id = jsonIO.load(filepath)
         reply: discord.PartialMessage = message.channel.get_partial_message(reply_id)
         await reply.delete()
         os.remove(filepath)
-    if not deleted: await message_embed.message_reply(message)
+    if not deleted:
+        await message_embed.message_reply(message)
 
 async def create_reply_json(id, reply_id):
     filepath = f"{DIR}/data/history/{reply_id}.json"
-    with open(filepath, "w") as file:
-        json.dump(id, file)
+    jsonIO.dump(filepath, id)
