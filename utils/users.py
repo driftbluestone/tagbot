954a885068cf54e648ddc9606e79c3509a8d2dcc
driftbluestone <driftbluestone@gmail.com>
2026-06-14 15:51:01 -0700

    grouop permission checking

--- utils/users.py
+++ utils/users.py
@@ -43,10 +43,15 @@ async def permission_check(user_id: int, permission: str) -> bool:
         return True
 
     # Ensure permission exists
-    if permission not in config.permissions_config:
-        raise KeyError(f"Permission not found: {permission}")
+    is_group = False
+    if "." in permission:
+        is_group = True
+        _group_check(permission)
+    else:
+        if permission not in config.permissions_config:
+            raise KeyError(f"Permission not found: {permission}")
 
-    # local user layer
+    # user layer
     user_profile = get_user_profile(user_id)
     try:
         profile_permission = user_profile["permissions"][permission]
@@ -73,7 +78,19 @@ async def permission_check(user_id: int, permission: str) -> bool:
             return role[permission]
 
     # default layer
-    return config.permissions_config[permission]["default_enabled"]
+    if is_group:
+        group, permission = permission.split(":")
+        return config.permissions_config[group][permission]["default_enabled"]
+    else:
+        return config.permissions_config[permission]["default_enabled"]
+
+async def _group_check(perm: str) -> bool:
+    group, permission = perm.split(":")
+
+    if group not in config.permissions_config:
+        raise KeyError(f"Group not found: {group}")
+    if permission not in config.permissions_config[group]:
+        raise KeyError(f"Permission within group not found: {group}:{permission}")
 
 def update_role(role_id):
     filepath = f"{DIR}/data/roles/{role_id}.json"
