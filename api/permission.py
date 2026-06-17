48001c5fe2aae80c8f172aca4c7d18e6a10c28af
driftbluestone <driftbluestone@gmail.com>
2026-06-16 17:52:40 -0700

    this is not prod ready 🤭

--- api/permission.py
+++ api/permission.py
@@ -79,6 +79,7 @@ class Group:
             self.name = name
             self.ext = ext()
         self.group = f"{self.ext}.{self.name}"
+        self.TLG = False
     
     def check(self, user_id: int, permission: str) -> bool:
         return check(user_id, f"{self.ext}.{self.name}:{permission}")
