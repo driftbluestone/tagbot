fd84de12df554df8f9061f7ac13672b600bb0ec1
driftbluestone <driftbluestone@gmail.com>
2026-07-18 23:06:33 -0700

    Revert "add group registration"
    This reverts commit 5a43a0fe4c6bbf1a4a729718b22f7b76782b3ce6.

--- api/permission.py
+++ api/permission.py
@@ -8,13 +8,7 @@ def create(name: str, display_name: str, toggleable: bool = True, default_enable
     """
     Register a new permission
     """
-    if "." in name:
-        raise ValueError("Permission names cannot contain '.'")
-    
-    if ":" in name:
-        extension, name = name.split(":")
-    else:
-        extension = ext()
+    extension = ext()
 
     id = f"{extension}:{name}"
     config.permissions_config[id] = {
@@ -52,22 +46,7 @@ def check(user_id: int, permission: str) -> bool:
         permission = f"{extension}:{permission}"
     return asyncio.create_task(users.permission_check(user_id, permission))
 
-def group(name: str):
-    """
-    Register a new permission group. Permissions within groups are saved as `namespace.group:permission`.
-    """
-    if ":" in name:
-        extension, name = name.split(":")
-    elif "." in name:
-        extension, name = name.split(":")
-    else:
-        extension = ext()
-    id = f"{extension}.{name}"
-    if id not in config.permissions_config():
-        config.permissions_config[id] = {}
-        config.save_permisions_config()
-        return True
-    return False
+def group(name: str): ...
 
 class Group: 
     def __init__(self, name: str):
@@ -76,19 +55,9 @@ class Group:
         else:
             self.name = name
             self.ext = ext()
-            self.group = f"{self.ext}:{self.name}"
     
     def check(self, user_id: int, permission: str) -> bool:
         return check(user_id, f"{self.ext}.{self.name}:{permission}")
     
     def create(self, name: str, display_name: str, toggleable: bool = True, default_enabled: bool = False, role_assignable: bool = True):
-        if name in config.permissions_config[self.group]:
-            return False
-        config.permissions_config[self.group][name] = {
-            "display_name": display_name,
-            "toggleable": toggleable,
-            "default_enabled": default_enabled,
-            "role_assignable": role_assignable
-        }
-        config.save_permisions_config()
-        return True
+        return create(f"{self.ext}.{self.name}:{name}", display_name, toggleable, default_enabled, role_assignable)
