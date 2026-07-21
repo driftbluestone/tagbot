5b29c5b60be71e80c58ef6acb02767f9d9fd0bf6
driftbluestone <driftbluestone@gmail.com>
2026-07-21 10:39:41 -0700

    i think it,, works now

--- utils/db.py
+++ utils/db.py
@@ -1,14 +1,12 @@
 import psycopg
 from psycopg import sql
+from typing import Any
 from utils.logger import Logger
 from utils.utils import data
 
 logger = Logger()
-
-__all__ = ["run", "query", "cursor", "connection", "check_connection", "close_connection"]
-
 data = data["DB"]
-SCHEMA = data["SCHEMA"]
+SCHEMA = sql.Identifier(data["SCHEMA"])
 HOST = data["HOST"]
 NAME = data["NAME"]
 USER = data["USER"]
@@ -51,33 +49,65 @@ def single(*args):
     connection.execute(*args)
     return cursor.fetchone()
 
-def insert(table: str, key: tuple[str], field: tuple[str], value: tuple[any]):
+def insert(table: str, key: tuple[str, ...], field: tuple[str, ...], value: tuple[Any, ...]):
+    if not isinstance(key, tuple):
+        raise ValueError("Argument `key` must be a tuple")
+    if not isinstance(field, tuple):
+        raise ValueError("Argument `field` must be a tuple")
+    if not isinstance(value, tuple):
+        raise ValueError("Argument `value` must be a tuple")
+    if len(key) + len(field) != len(value):
+        raise ValueError("len(key) + len(field) must equal len(value)")
+    if not key:
+        raise ValueError("Arguments `key`, `field`, and `value` cannot be empty")
+    
     query = sql.SQL("INSERT INTO {schema}.{table} ({fields}) VALUES ({values}) ON CONFLICT ({keys}) DO UPDATE SET {assignments}").format(
         schema = SCHEMA,
-        table = table,
-        fields = sql.SQL(", ").join(sql.Identifier(f) for f in key + field),
+        table = sql.Identifier(table),
+        fields = sql.SQL(", ").join(sql.Identifier(f) for f in (key + field)),
         values = sql.SQL(", ").join(sql.Placeholder() for _ in value),
         keys = sql.SQL(", ").join(sql.Identifier(k) for k in key),
-        assignments = sql.SQL(", ").join(sql.SQL("{f} = EXCLUDED.{f}").format(f=sql.Identifier(f)) for f in field if f not in key)
+        assignments = sql.SQL(", ").join(sql.SQL("{f} = EXCLUDED.{f}").format(
+            f=sql.Identifier(f)) for f in field if f not in key
+        )
     )
     run(query, value)
 
-def delete(table: str, key: str, value: any):
-    run(f"DELETE FROM {SCHEMA}.{table} WHERE {key} = %s", (value,))
+def delete(table: str, key: tuple[str, ...], value: tuple[Any, ...]):
+    if not isinstance(key, tuple):
+        raise ValueError("Argument `key` must be a tuple")
+    if not isinstance(value, tuple):
+        raise ValueError("Argument `value` must be a tuple")
+    if len(key) != len(value):
+        raise ValueError("Arguments `key` and `value` must be equal in length")
+    if not key:
+        raise ValueError("Arguments `key` and `value` cannot be empty")
+    
+    query = sql.SQL("DELETE FROM {schema}.{table} WHERE {key}").format(
+        schema = SCHEMA,
+        table = sql.Identifier(table),
+        key = sql.SQL(" AND ").join(sql.SQL("{k} = %s").format(
+            k = sql.Identifier(k)) for k in key
+        )
+    )
+    run(query, value)
 
 def get(table: str, value: tuple[any], key: tuple[str] = ("id",), column: tuple[str] = ("*",)):
+    if not isinstance(key, tuple):
+        raise ValueError("Argument `key` must be a tuple")
+    if not isinstance(value, tuple):
+        raise ValueError("Argument `value` must be a tuple")
+    if len(key) != len(value):
+        raise ValueError("Arguments `key` and `value` must be equal in length")
+    if not key:
+        raise ValueError("Arguments `key` and `value` cannot be empty")
+    
     query = sql.SQL("SELECT {column} FROM {schema}.{table} WHERE {key}").format(
-        column = sql.SQL(", ").join(sql.Identifier(c) for c in column),
         schema = SCHEMA,
-        table = table,
-        key = sql.SQL(" AND ").join(sql.SQL("{k} = %s").format(k=sql.Identifier(k)) for k in key)
+        table = sql.Identifier(table),
+        column = sql.SQL(", ").join(sql.Identifier(c) for c in column),
+        key = sql.SQL(" AND ").join(sql.SQL("{k} = %s").format(
+            k = sql.Identifier(k)) for k in key
+        )
     )
-    run(query, value)
-# def get(table: str, value: str | tuple[str], column: str | tuple[str] = "*", key: str | tuple[str] = "id") -> tuple[any]:
-#     query = sql.SQL("SELECT {column} FROM {schema}.{table} WHERE {key}").format(
-#         column = sql.SQL(", ").join(sql.Identifier(c) for c in column) if isinstance(column, tuple) else sql.Identifier(column),
-#         schema = SCHEMA,
-#         table = table,
-#         key = sql.SQL(" AND ").join(sql.SQL("{k} = %s").format(k=sql.Identifier(k)) for k in key) if isinstance(key, tuple) else sql.SQL("{key} = %s").format(key=sql.Identifier(key))
-#     )
-#     run(query, (value,) if not isinstance(value, tuple) else value)
+    return single(query, value)
