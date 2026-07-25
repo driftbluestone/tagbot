19265cd5d1b66bb532fc7b0461d3d943ebf30174
driftbluestone <driftbluestone@gmail.com>
2026-07-25 16:55:43 -0700

    ok now for rewriting cogs

--- db/server.py
+++ db/server.py
@@ -1,10 +1,8 @@
-from typing import Literal
+from typing import Literal, Any
 from db import db
 
-def server(server_id: int, fields: tuple[Literal["perms", "extensions", "default_user_data","command_prefix", "*"], ...]):
-    """
-    
-    """
-    if isinstance(fields, str):
-        fields = (fields,)
+def get_server(server_id: int, fields: tuple[Literal["perms", "extensions", "default_user_data","command_prefix", "*"], ...]):
     return db.get("server", (server_id,), ("server_id",), fields)
+
+def update_server(server_id: int, fields: tuple[Literal["perms", "extensions", "default_user_data","command_prefix", "*"], ...], value: tuple[Any]):
+    db.insert("server", ("server_id"), fields, (server_id,) + value)
