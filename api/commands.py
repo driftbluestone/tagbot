bad11e2fa720081f9793f3e33c5ffeb3f63e5b9d
driftbluestone <driftbluestone@gmail.com>
2026-07-26 15:19:13 -0700

    command check to replace cog_check

--- api/commands.py
+++ api/commands.py
@@ -2,20 +2,7 @@ import discord, functools
 from discord.ext import commands
 from discord.utils import MISSING
 from typing import Callable
-from psycopg import sql
-from db import db
-
-def _extension_enabled(extension: str, server_id: int) -> bool:
-    query = sql.SQL("""SELECT * FROM {schema}.server
-                            WHERE {extension} = ANY(extensions)
-                            AND server_id = {server_id}
-                            """).format(
-                schema = db.SCHEMA,
-                extension = sql.Placeholder(),
-                server_id = sql.Placeholder()
-                )
-    value, = db.single(query, (extension, server_id))
-    return value
+from db import server
 
 def _get_server_id(*args) -> int:
     for arg in args:
@@ -26,8 +13,22 @@ def _get_server_id(*args) -> int:
 
 class Cog(commands.Cog):
     async def cog_check(self, ctx: commands.Context):
+        """Do not override this function, use `command_check()` instead"""
+        if not self.command_check(ctx):
+            return False
         extension = self.__module__.split(".")[1]
-        return _extension_enabled(extension, ctx.guild.id)
+        return server.check_extension(ctx.guild.id, extension)
+
+    async def command_check(self, ctx) -> bool:
+        """Docstring copied from discord.py. Copyright (c) 2015-present Rapptz
+
+        A special method that registers as a :func:`~discord.ext.commands.check`
+        for every command and subcommand in this cog.
+    
+        This function **can** be a coroutine and must take a sole parameter,
+        ``ctx``, to represent the :class:`.Context`.
+        """
+        return True
     
     @classmethod
     def listener(cls, name: str = MISSING):
@@ -36,7 +37,7 @@ class Cog(commands.Cog):
             @functools.wraps(func)
             async def wrapper(self, *args, **kwargs):
                 server_id = _get_server_id(*args)
-                if not _extension_enabled(extension, server_id):
+                if not server.check_extension(server_id, extension):
                     return
                 return await func(self, *args, **kwargs)
             return super(Cog, cls).listener(name)(wrapper)
